2ae957f7bc419b886d2624a23b80c948c07ee142
a.pride <a.pride.msk@gmail.com>
2020-09-23 09:53:28 +0300

    Initial Commit

---
+++ VKNewsFeed/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  VKNewsFeed
+//
+//  Created by Александр Прайд on 23.09.2020.
+//  Copyright © 2020 Alexander Pride. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
